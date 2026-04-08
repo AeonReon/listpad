7d633ae746c7ee35af0abd77ab4490d710d6914c
Ai Automator <aiautomator@Ais-Mac-mini.local>
2026-04-08 13:17:28 +0100

    Blue and gold redesign, master/detail navigation, real app icon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'listpad-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
+const CACHE = 'listpad-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/images/ListPad.png'];
 
 self.addEventListener('install', e => {
     e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
